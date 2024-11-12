bd23339e1631c8308ce1bde6ba51736b59266c16
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-11-12 12:33:33 +0300

    Update tg_quiz_1_2.py

--- tg_quiz_1_2.py
+++ tg_quiz_1_2.py
@@ -1,10 +1,4 @@
-"""
-Работает с этими модулями:
-python-telegram-bot==13.15
-redis==3.2.1
-"""
 import os
-import logging
 import redis
 import requests
 from dotenv import load_dotenv
@@ -14,7 +8,6 @@ from telegram.ext import Filters, Updater
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
 
 _database = None
-
 
 def start(update, context):
     load_dotenv()
@@ -31,38 +24,33 @@ def start(update, context):
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text(text='Привет!', reply_markup=reply_markup)
+    update.message.reply_text(text='Сейчас ты в START', reply_markup=reply_markup)
     return "HANDLE_MENU"
 
 
 def handle_menu(update, context):
-    users_reply = update.message.text
-    update.message.reply_text(users_reply)
+    update.message.reply_text('Сейчас ты в HANDLE_MENU')
     return "START"
 
+def text_button(update, context):
+    # query = update.callback_query
+    # query.data
+    update.message.reply_text('Сейчас ты в text_button')
+    return 'HANDLE_MENU'
 
 def handle_users_reply(update, context):
-    """
-    Функция, которая запускается при любом сообщении от пользователя и решает как его обработать.
-    Эта функция запускается в ответ на эти действия пользователя:
-        * Нажатие на inline-кнопку в боте
-        * Отправка сообщения боту
-        * Отправка команды боту
-    Она получает стейт пользователя из базы данных и запускает соответствующую функцию-обработчик (хэндлер).
-    Функция-обработчик возвращает следующее состояние, которое записывается в базу данных.
-    Если пользователь только начал пользоваться ботом, Telegram форсит его написать "/start",
-    поэтому по этой фразе выставляется стартовое состояние.
-    Если пользователь захочет начать общение с ботом заново, он также может воспользоваться этой командой.
-    """
+
     db = get_database_connection()
     if update.message:
         user_reply = update.message.text
         chat_id = update.message.chat_id
-        print('user_reply', user_reply)
+        #здесь поставить функцию handle_menu?
+
     elif update.callback_query:
         user_reply = update.callback_query.data
         chat_id = update.callback_query.message.chat_id
-        print('user_reply', user_reply)
+        #здесь поставить функцию handle_menu?
+
 
     else:
         return
@@ -76,9 +64,7 @@ def handle_users_reply(update, context):
         'HANDLE_MENU': handle_menu
     }
     state_handler = states_functions[user_state]
-    # Если вы вдруг не заметите, что python-telegram-bot перехватывает ошибки.
-    # Оставляю этот try...except, чтобы код не падал молча.
-    # Этот фрагмент можно переписать.
+
     try:
         next_state = state_handler(update, context)
         print('next_state', next_state)
@@ -88,9 +74,6 @@ def handle_users_reply(update, context):
 
 
 def get_database_connection():
-    """
-    Возвращает конекшн с базой данных Redis, либо создаёт новый, если он ещё не создан.
-    """
     global _database
     if _database is None:
         database_password = os.getenv("DATABASE_PASSWORD")
@@ -98,7 +81,6 @@ def get_database_connection():
         database_port = os.getenv("DATABASE_PORT")
         _database = redis.Redis(host=database_host, port=database_port, password=database_password)
     return _database
-
 
 if __name__ == '__main__':
     load_dotenv()
