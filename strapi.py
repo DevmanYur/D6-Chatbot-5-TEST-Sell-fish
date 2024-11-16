46ef527614de4febee1d5b3daa99fb078b4a2ec4
DevmanYur <devmanyur@gmail.com>
2024-11-16 18:33:58 +0300

    Update strapi.py

--- strapi.py
+++ strapi.py
@@ -2,23 +2,31 @@ import os
 from pprint import pprint
 import requests
 from dotenv import load_dotenv
-
+from io import BytesIO
+from PIL import Image
 load_dotenv()
 
-strapi_token = os.getenv("STRAPI_TOKEN")
-headers = {'Authorization': f'Bearer {strapi_token}'}
-response0 = requests.get(f'http://localhost:1337/api/products/vf9v1zcox2ugiha5zkaqgnm7?populate=picture',headers=headers)
-product = response0.json()
+strapi_tokenq = os.getenv("STRAPI_TOKEN")
+headersq = {'Authorization': f'Bearer {strapi_tokenq}'}
+response0 = requests.get(f'http://localhost:1337/api/products/vf9v1zcox2ugiha5zkaqgnm7?populate=picture',headers=headersq)
+productq = response0.json()
 
-url_pic = product['data']['picture']['formats']['thumbnail']['url']
+url_picq = productq['data']['picture']['formats']['thumbnail']['url']
 
-name_photo = product['data']['picture']['formats']['thumbnail']['name']
-response = requests.get(f'http://localhost:1337{url_pic}')
-response.raise_for_status()
+name_photo = productq['data']['picture']['formats']['thumbnail']['name']
+# name_photo = 'ddqq'
+response22 = requests.get(f'http://localhost:1337{url_picq}')
+response22.raise_for_status()
 
-photo_path = f'{name_photo}'
-with open(photo_path, 'wb') as file:
-    file.write(response.content)
+image_data22 = BytesIO(response22.content)
+img = Image.open(image_data22)
+img.save('img1.png')
+
+
+
+# photo_path = f'{name_photo}'
+# with open(photo_path, 'wb') as file:
+#     file.write(img)
 
 
 
