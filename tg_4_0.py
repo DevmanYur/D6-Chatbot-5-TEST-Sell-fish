f6971ccc84505f41870d2c1e866d85afe1a97f45
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-11-21 16:42:59 +0300

    Update tg_4_0.py

--- tg_4_0.py
+++ tg_4_0.py
@@ -110,32 +110,14 @@ def handle_description(update, context):
 
 
 
-    keyboard = [[
-        InlineKeyboardButton("Назад", callback_data='Нажата кнопка Назад')
-    ]]
+    keyboard = [
+        [InlineKeyboardButton("Добавить в корзину", callback_data='Нажата кнопка Добавить в корзину')],
+        [InlineKeyboardButton("Назад", callback_data='Нажата кнопка Назад')]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     query.answer()
     description = product['data']['description']
-
-    ####555
-    strapi_tokenq55 = os.getenv("STRAPI_TOKEN")
-    headersq55 = {'Authorization': f'Bearer {strapi_tokenq55}'}
-    response055 = requests.get(f'http://localhost:1337/api/products/zn17dtr0wv00kq32i0y8b3n1?populate=picture',
-                               headers=headersq55)
-    productq55 = response055.json()
-
-    url_picq55 = productq55['data']['picture']['formats']['thumbnail']['url']
-
-    name_photo55 = productq55['data']['picture']['formats']['thumbnail']['name']
-    # name_photo = 'ddqq'
-    response2255 = requests.get(f'http://localhost:1337{url_picq55}')
-    response2255.raise_for_status()
-
-    image_data2255 = BytesIO(response2255.content)
-    image_data = Image.open(image_data2255)
-    ####555
-
 
 
     context.bot.send_document(chat_id=update.callback_query.message.chat_id, document=open('img3.png', 'rb'),
