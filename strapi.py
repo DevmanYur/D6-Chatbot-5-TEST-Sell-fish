44b699ac30d924586fb25c84afe2211bf1ea5fa1
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-11-22 11:40:35 +0300

    Update strapi.py

--- strapi.py
+++ strapi.py
@@ -37,14 +37,14 @@ def f2():
     pprint(productq66)
 
 def f3():
-    strapi_tokenq66 = os.getenv("STRAPI_TOKEN")
-    headersq66 = {'Authorization': f'Bearer {strapi_tokenq66}'}
+    strapi_tokenq667 = os.getenv("STRAPI_TOKEN")
+    headersq667 = {'Authorization': f'Bearer {strapi_tokenq667}'}
 
-    data = {'data': {'tg_id': '510011'}}
+    data = {'data': {'tg_id': '006'}}
 
-    response066 = requests.post(f'http://localhost:1337/api/carts', headers=headersq66, json = data)
-    productq66 = response066.json()
-    pprint(productq66)
+    response0667 = requests.post(f'http://localhost:1337/api/carts', headers=headersq667, json = data)
+    productq667 = response0667.json()
+    pprint(productq667)
 
 '''
 {'data': [{'createdAt': '2024-11-21T13:29:51.740Z',
@@ -58,7 +58,36 @@ def f3():
 
 '''
 
-f3()
+def f4():
+    'GET /api/users?filters[username][$eq]=John'
+    strapi_tokenq66 = os.getenv("STRAPI_TOKEN")
+    headersq66 = {'Authorization': f'Bearer {strapi_tokenq66}'}
+    tg_id = '510011'
+    tg_id_for_strapi = f'tg_id_{tg_id}'
+    response066 = requests.get(f'http://localhost:1337/api/carts?filters[tg_id][$eq]={tg_id_for_strapi}', headers=headersq66)
+    productq66 = response066.json()
+
+    if productq66['data']:
+        print("Блок А")
+        print("здесь что то есть")
+        print('documentId :', productq66['data'][0]['tg_id'])
+        print('documentId :' , productq66['data'][0]['documentId'])
+    else:
+        print("Блок Б")
+        print("здесь пусто")
+        print("создаем новый ")
+
+        strapi_tokenq667 = os.getenv("STRAPI_TOKEN")
+        headersq667 = {'Authorization': f'Bearer {strapi_tokenq667}'}
+
+        data = {'data': {'tg_id': tg_id_for_strapi}}
+
+        response0667 = requests.post(f'http://localhost:1337/api/carts', headers=headersq667, json=data)
+        productq667 = response0667.json()
+        print("Теперь: ")
+        pprint(productq667)
+
+
 
 
 # photo_path = f'{name_photo}'
@@ -67,7 +96,7 @@ f3()
 
 
 
-
+f4()
 
 
 
